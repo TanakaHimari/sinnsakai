368002c324e805296a896084860bb12ee70fd090
陽 <youwanashityu@gmail.com>
2025-09-01 13:08:30 +0900

    [fix]キャンバスサイズの微調整

--- Assets/Script/StageGenerator.cs
+++ Assets/Script/StageGenerator.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections.Generic;
+
 using UnityEngine.Rendering;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 
 public class StageGenerator : MonoBehaviour
